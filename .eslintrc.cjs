b592c4829b778745d1a6904dd33bae8fc0d83402
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-11-25 10:10:03 -0300

    refactor: Switch from spaces to tabs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-    ecmaVersion: 2020,
-    extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
-  },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
-    },
-  ],
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
 };
